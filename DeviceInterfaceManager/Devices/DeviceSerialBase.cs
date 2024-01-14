e378c0ecbda7ef7670379768a782c12463f0d908
Nils Schmitt <oudoumo@yahoo.de>
2024-01-14 01:41:57 +0100

    Added IDeviceSerial interface to DeviceSerialBase.cs

--- DeviceInterfaceManager/Devices/DeviceSerialBase.cs
+++ DeviceInterfaceManager/Devices/DeviceSerialBase.cs
@@ -3,7 +3,38 @@ using System.Threading.Tasks;
 
 namespace DeviceInterfaceManager.Devices;
 
-public abstract class DeviceSerialBase
+public class DeviceSerialBase : IDeviceSerial
 {
+    public ComponentInfo Switch { get; } = new(1, 255);
+    public event EventHandler<InputChangedEventArgs>? InputChanged;
+    public ComponentInfo Led { get; } = new(1, 255);
+    public ComponentInfo Dataline { get; } = new(1, 255);
+    public ComponentInfo SevenSegment { get; } = new(1, 255);
+    public Task SetLedAsync(string position, bool isEnabled)
+    {
+        return Task.CompletedTask;
+    }
 
+    public Task SetDatalineAsync(string position, bool isEnabled)
+    {
+        return Task.CompletedTask;
+    }
+
+    public Task SetSevenSegmentAsync(string position, string data)
+    {
+        return Task.CompletedTask;
+    }
+
+    public string BoardName => "Debug";
+    public string SerialNumber => "000000";
+
+    public Task<ConnectionStatus> ConnectAsync()
+    {
+        return Task.FromResult(ConnectionStatus.Connected);
+    }
+
+    public void Disconnect()
+    {
+
+    }
 }
