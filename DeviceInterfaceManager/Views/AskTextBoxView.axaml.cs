94a9a395aca31dbd69bd45d19e7a18942cd09979
Nils Schmitt <oudoumo@yahoo.de>
2024-01-31 20:59:01 +0100

    Added AskTextBoxView.axaml.cs

---
+++ DeviceInterfaceManager/Views/AskTextBoxView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DeviceInterfaceManager.Views;
+
+public partial class AskTextBoxView : UserControl
+{
+    public AskTextBoxView()
+    {
+        InitializeComponent();
+    }
+}
