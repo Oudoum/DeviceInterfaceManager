6a42a73454a3dea4f28288f767e22791d28bc679
Nils Schmitt <oudoumo@yahoo.de>
2024-01-31 20:59:14 +0100

    Added AskTextBoxViewModel.cs

---
+++ DeviceInterfaceManager/ViewModels/AskTextBoxViewModel.cs
@@ -0,0 +1,10 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DeviceInterfaceManager.ViewModels;
+
+public class AskTextBoxViewModel : ObservableObject
+{
+    public string? Title { get; set; }
+    
+    public string? Text { get; set; }
+}
