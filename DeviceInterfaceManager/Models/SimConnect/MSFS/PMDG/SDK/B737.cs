e77b4a0dccdd9403661cdf217166ea509fca8c81
Nils Schmitt <oudoumo@yahoo.de>
2024-04-06 23:12:02 +0200

    Move Mouse into Mouse.cs Small change to Cdu struct Made class static

--- DeviceInterfaceManager/Models/SimConnect/MSFS/PMDG/SDK/B737.cs
+++ DeviceInterfaceManager/Models/SimConnect/MSFS/PMDG/SDK/B737.cs
@@ -12,7 +12,7 @@ using System.Runtime.InteropServices;
 
 namespace DeviceInterfaceManager.Models.SimConnect.MSFS.PMDG.SDK;
 
-public class B737
+public static class B737
 {
     public const string DataName = "PMDG_NG3_Data";
     public const string ControlName = "PMDG_NG3_Control";
@@ -545,32 +545,9 @@ public class B737
 
 
     [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi, Pack = 4)]
-    public readonly struct Cdu : Models.SimConnect.MSFS.PMDG.SDK.Cdu.ICduScreen
+    public struct Cdu : Models.SimConnect.MSFS.PMDG.SDK.Cdu.ICduScreen
     {
-        public Models.SimConnect.MSFS.PMDG.SDK.Cdu.Screen Screen { get; }
-    }
-
-    //Mouse Flag
-    public enum Mouse : uint
-    {
-        RightSingle = 0x80000000u,
-        MiddleSingle = 0x40000000u,
-        LeftSingle = 0x20000000u,
-        RightDouble = 0x10000000u,
-        MiddleDouble = 0x08000000u,
-        LeftDouble = 0x04000000u,
-        RightDrag = 0x02000000u,
-        MiddleDrag = 0x01000000u,
-        LeftDrag = 0x00800000u,
-        Move = 0x00400000u,
-        DownRepeat = 0x00200000u,
-        RightRelease = 0x00080000u,
-        MiddleRelease = 0x00040000u,
-        LeftRelease = 0x00020000u,
-        WheelFlip = 0x00010000u, // invert direction of mouse wheel
-        WheelSkip = 0x00008000u, // look at next 2 rect for mouse wheel commands
-        WheelUp = 0x00004000u,
-        WheelDown = 0x00002000u,
+        public Models.SimConnect.MSFS.PMDG.SDK.Cdu.Screen Screen { get; set; }
     }
 
     // Audio control panel selected receiver flags.
