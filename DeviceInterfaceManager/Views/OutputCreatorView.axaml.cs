f7166bcae01e255dd5c4abaf7b0645e541101582
Nils Schmitt <oudoumo@yahoo.de>
2024-04-06 23:37:52 +0200

    Create OutputCreatorView.axaml.cs

---
+++ DeviceInterfaceManager/Views/OutputCreatorView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Input;
+
+
+namespace DeviceInterfaceManager.Views;
+
+public partial class OutputCreatorView : UserControl
+{
+    public OutputCreatorView()
+    {
+        InitializeComponent();
+    }
+}
