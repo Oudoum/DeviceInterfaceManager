e0ca88a68dfff338f133e2b00dedbaa5cd68e942
Nils Schmitt <oudoumo@yahoo.de>
2024-04-06 23:45:28 +0200

    Add return of ConvertBack in NullableKeyValuePairConverter.cs

--- DeviceInterfaceManager/Converters/NullableKeyValuePairConverter.cs
+++ DeviceInterfaceManager/Converters/NullableKeyValuePairConverter.cs
@@ -34,8 +34,8 @@ public class NullableKeyValuePairConverter : IValueConverter
         };
     }
 
-    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
     {
-        throw new NotImplementedException();
+        return value;
     }
 }
