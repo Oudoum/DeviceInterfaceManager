18450efe05af21038ba89fb8235568f26643e1fb
Nils Schmitt <oudoumo@yahoo.de>
2024-04-06 23:38:14 +0200

    Create PreconditionModel.cs

---
+++ DeviceInterfaceManager/Models/PreconditionModel.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DeviceInterfaceManager.Models;
+
+public partial class PreconditionModel : Precondition
+{
+    [ObservableProperty]
+    private string? _description;
+
+    [ObservableProperty]
+    private bool _hasError;
+
+    public PreconditionModel(OutputCreator outputCreator)
+    {
+        IsActive = true;
+        Operator = '=';
+        ReferenceId = outputCreator.Id;
+        Description = outputCreator.Description;
+    }
+
+    public PreconditionModel(IPrecondition precondition, IEnumerable<OutputCreator> outputCreators)
+    {
+        IsActive = precondition.IsActive;
+        Operator = precondition.Operator;
+        ComparisonValue = precondition.ComparisonValue;
+        IsOrOperator = precondition.IsOrOperator;
+
+        OutputCreator? matchingOutputCreator = outputCreators.FirstOrDefault(oc => oc.Id == precondition.ReferenceId);
+        if (matchingOutputCreator is not null)
+        {
+            Description = matchingOutputCreator.Description;
+            ReferenceId = precondition.ReferenceId;
+            return;
+        }
+        HasError = true;
+    }
+}
