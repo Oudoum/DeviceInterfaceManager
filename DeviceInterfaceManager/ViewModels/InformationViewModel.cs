6cb7a37f433e06ac98b04dfc89a8f67feedd4f4c
Nils Schmitt <oudoumo@yahoo.de>
2024-05-09 23:28:14 +0200

    Remove information strings

--- DeviceInterfaceManager/ViewModels/InformationViewModel.cs
+++ DeviceInterfaceManager/ViewModels/InformationViewModel.cs
@@ -6,20 +6,4 @@ namespace DeviceInterfaceManager.ViewModels;
 public class InformationViewModel(IInputOutputDevice inputOutputDevice) : ObservableObject
 {
     public IInputOutputDevice InputOutputDevice => inputOutputDevice;
-
-    public string ButtonInformation => $"{InputOutputDevice.Switch.Count} | " +
-                                       $"( {InputOutputDevice.Switch.First} - " +
-                                       $"{InputOutputDevice.Switch.Last} )";
-
-    public string LedInformation => $"{InputOutputDevice.Led.Count} | " +
-                                    $"( {InputOutputDevice.Led.First} - " +
-                                    $"{InputOutputDevice.Led.Last} )";
-
-    public string DatalineInformation => $"{InputOutputDevice.Dataline.Count} | " +
-                                         $"( {InputOutputDevice.Dataline.First} - " +
-                                         $"{InputOutputDevice.Dataline.Last} )";
-
-    public string SevenSegmentInformation => $"{InputOutputDevice.SevenSegment.Count} | " +
-                                             $"( {InputOutputDevice.SevenSegment.First} - " +
-                                             $"{InputOutputDevice.SevenSegment.Last} )";
 }
