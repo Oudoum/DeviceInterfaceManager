78a1a7345719a94f71d618f632907f9ad94352a4
Nils Schmitt <oudoumo@yahoo.de>
2024-05-09 23:34:10 +0200

    Add on pointer pressed methode

--- DeviceInterfaceManager/Views/MainWindow.axaml.cs
+++ DeviceInterfaceManager/Views/MainWindow.axaml.cs
@@ -1,11 +1,27 @@
-﻿using FluentAvalonia.UI.Windowing;
+﻿using Avalonia.Controls;
+using Avalonia.Input;
+using DeviceInterfaceManager.Models.Devices;
+using FluentAvalonia.UI.Windowing;
 
 namespace DeviceInterfaceManager.Views;
 
 public partial class MainWindow : AppWindow
 {
+    
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void InputElement_OnPointerPressed(object? sender, PointerPressedEventArgs e)
+    {
+        if (e.KeyModifiers != KeyModifiers.Control || sender is not StackPanel { DataContext: not null } stackPanel)
+        {
+            return;
+        }
+        
+        DataObject data = new();
+        data.Set(nameof(IInputOutputDevice), stackPanel.DataContext);
+        DragDrop.DoDragDrop(e, data, DragDropEffects.Link);
     }
 }
