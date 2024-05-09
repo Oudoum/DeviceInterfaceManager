e2a51f2bca3af01ebb0dd10f3e6684f7dee17734
Nils Schmitt <oudoumo@yahoo.de>
2024-05-09 23:32:10 +0200

    Add empty constructor

--- DeviceInterfaceManager/ViewModels/InputTestViewModel.cs
+++ DeviceInterfaceManager/ViewModels/InputTestViewModel.cs
@@ -5,5 +5,12 @@ namespace DeviceInterfaceManager.ViewModels;
 
 public class InputTestViewModel(IInputOutputDevice inputOutputDevice) : ObservableObject
 {
+    #if DEBUG
+    public InputTestViewModel() : this(new DeviceSerialBase())
+    {
+        
+    }
+    #endif
+    
     public IInputOutputDevice InputOutputDevice => inputOutputDevice;
 }
