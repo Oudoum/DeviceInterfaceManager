60066a23b5b24ea1973c2adc687951f178c92185
Nils Schmitt <oudoumo@yahoo.de>
2024-05-09 23:38:44 +0200

    Add Velopack

--- DeviceInterfaceManager/Program.cs
+++ DeviceInterfaceManager/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Avalonia;
 using System;
+using Velopack;
 
 namespace DeviceInterfaceManager;
 
@@ -9,8 +10,11 @@ internal static class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        VelopackApp.Build().Run();
+        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+    }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     private static AppBuilder BuildAvaloniaApp()
