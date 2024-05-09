61d5ea5495fb6306f476d721ac5a4d0cad3ed8c7
Nils Schmitt <oudoumo@yahoo.de>
2024-05-09 23:26:42 +0200

    Add drop handler

--- DeviceInterfaceManager/Views/HomeView.axaml.cs
+++ DeviceInterfaceManager/Views/HomeView.axaml.cs
@@ -1,4 +1,6 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Input;
+using DeviceInterfaceManager.Models.Devices;
 
 namespace DeviceInterfaceManager.Views;
 
@@ -7,5 +9,17 @@ public partial class HomeView : UserControl
     public HomeView()
     {
         InitializeComponent();
+        
+        MyButton.AddHandler(DragDrop.DropEvent, Drop);
+    }
+    
+    private void Drop(object? sender, DragEventArgs e)
+    {
+        object? data = e.Data.Get(nameof(IInputOutputDevice));
+
+        if (data is IInputOutputDevice inputOutputDevice)
+        {
+            MyButton.Content = inputOutputDevice;
+        }
     }
 }
