690c93f5c71cb4e91aa4b0e9b4d6c024f484b1ae
Nils Schmitt <oudoumo@yahoo.de>
2024-05-09 23:38:12 +0200

    Add helper methode

--- DeviceInterfaceManager/Models/Settings.cs
+++ DeviceInterfaceManager/Models/Settings.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel;
+using System.IO;
+using System.Text.Json;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace DeviceInterfaceManager.Models;
 
 public sealed partial class Settings : ObservableObject
 {
+    private static readonly string SettingsFile = Path.Combine(App.UserDataPath, "settings.json");
+
     [ObservableProperty]
     private bool _minimizedHide;
 
@@ -13,6 +18,36 @@ public sealed partial class Settings : ObservableObject
     [ObservableProperty]
     private bool _isP3D;
 
-   [ObservableProperty]
-   private bool _fdsUsb;
+    [ObservableProperty]
+    private bool _fdsUsb;
+    
+    public static Settings CreateSettings()
+    {
+        if (!File.Exists(SettingsFile))
+        {
+            return new Settings();
+        }
+
+        try
+        {
+            return JsonSerializer.Deserialize<Settings>(File.ReadAllText(SettingsFile)) ?? new Settings();
+        }
+        catch
+        {
+            return new Settings();
+        }
+    }
+
+    private void SaveSettings()
+    {
+        string serialize = JsonSerializer.Serialize(this);
+        File.WriteAllText(SettingsFile, serialize);
+    }
+
+    protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+    {
+        base.OnPropertyChanged(e);
+        
+        SaveSettings();
+    }
 }
