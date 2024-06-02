936c24977ce9f88af1a8f18a0876bacbc0c47b3e
Nils Schmitt <oudoumo@yahoo.de>
2024-06-02 21:49:08 +0200

    Add public keyword

--- DeviceInterfaceManager/Models/Devices/IInput.cs
+++ DeviceInterfaceManager/Models/Devices/IInput.cs
@@ -4,9 +4,9 @@ namespace DeviceInterfaceManager.Models.Devices;
 
 public interface IInput
 {
-    ComponentInfo Switch { get; }
+    public ComponentInfo Switch { get; }
     
-    event EventHandler<InputChangedEventArgs> InputChanged;
+    public event EventHandler<InputChangedEventArgs> InputChanged;
 }
 
 public class InputChangedEventArgs(int position, bool isPressed) : EventArgs
