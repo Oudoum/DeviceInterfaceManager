0c5437962052ff1baa53a7e54cb0eb6e1463a6e9
Nils Schmitt <oudoumo@yahoo.de>
2024-06-27 22:14:09 +0200

    Add analog out

--- DeviceInterfaceManager/Models/Devices/IOutput.cs
+++ DeviceInterfaceManager/Models/Devices/IOutput.cs
@@ -7,12 +7,15 @@ public interface IOutput
     public ComponentInfo Led { get; }
     public ComponentInfo Dataline { get; }
     public ComponentInfo SevenSegment { get; }
+    public ComponentInfo AnalogOut { get; }
     
     public Task SetLedAsync(string? position, bool isEnabled);
     
     public Task SetDatalineAsync(string? position, bool isEnabled);
     
     public Task SetSevenSegmentAsync(string? position, string data);
+
+    public Task SetAnalogAsync(string? position, int value);
 
     public Task ResetAllOutputsAsync();
 }
