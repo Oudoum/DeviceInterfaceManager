cfc4af51cfab7173919e9d546be0285c7439f533
Nils Schmitt <oudoumo@yahoo.de>
2024-06-27 22:08:35 +0200

    Add analog in

--- DeviceInterfaceManager/Models/Devices/IInput.cs
+++ DeviceInterfaceManager/Models/Devices/IInput.cs
@@ -5,12 +5,20 @@ namespace DeviceInterfaceManager.Models.Devices;
 public interface IInput
 {
     public ComponentInfo Switch { get; }
-    
-    public event EventHandler<InputChangedEventArgs> InputChanged;
+    public ComponentInfo AnalogIn { get; }
+
+    public event EventHandler<SwitchPositionChangedEventArgs> SwitchPositionChanged;
+    public event EventHandler<AnalogInValueChangedEventArgs> AnalogInValueChanged;
 }
 
-public class InputChangedEventArgs(int position, bool isPressed) : EventArgs
+public class SwitchPositionChangedEventArgs(int position, bool isPressed) : EventArgs
 {
     public readonly int Position = position;
     public readonly bool IsPressed = isPressed;
+}
+
+public class AnalogInValueChangedEventArgs(int position, int value) : EventArgs
+{
+    public readonly int Position = position;
+    public readonly int Value = value;
 }
