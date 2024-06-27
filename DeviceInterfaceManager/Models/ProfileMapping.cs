d082090668bb3b6fde3944e4b8cc17efbb26cd08
Nils Schmitt <oudoumo@yahoo.de>
2024-06-27 22:10:45 +0200

    Change IsActive to an ObservableProperty

--- DeviceInterfaceManager/Models/ProfileMapping.cs
+++ DeviceInterfaceManager/Models/ProfileMapping.cs
@@ -1,14 +1,17 @@
 using CommunityToolkit.Mvvm.ComponentModel;
 
+#pragma warning disable CS0657 // Not a valid attribute location for this declaration
+
 namespace DeviceInterfaceManager.Models;
 
 public partial class ProfileMapping : ObservableObject
 {
-    public bool IsActive { get; set; } = true;
+    [ObservableProperty]
+    private bool _isActive = true;
 
     [ObservableProperty]
     private string? _profileName;
-    
+
     [ObservableProperty]
     private string? _id;
 
