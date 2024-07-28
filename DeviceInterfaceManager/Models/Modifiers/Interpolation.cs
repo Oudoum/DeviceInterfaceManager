7a35c8f3f836c562c7534358d8e35a9fb6f2015a
Nils Schmitt <oudoumo@yahoo.de>
2024-07-28 20:37:01 +0200

    Add Interpolation.cs modifier (not implemented)

---
+++ DeviceInterfaceManager/Models/Modifiers/Interpolation.cs
@@ -0,0 +1,15 @@
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DeviceInterfaceManager.Models.Modifiers;
+
+public partial class Interpolation : ObservableObject, IModifier
+{
+    [ObservableProperty]
+    private bool _isActive = true;
+    
+    public void Apply(ref StringBuilder value)
+    {
+        
+    }
+}
