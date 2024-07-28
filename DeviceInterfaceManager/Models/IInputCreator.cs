3a0dfd2a805e42e7f6715f1e5af1e0277b0226f7
Nils Schmitt <oudoumo@yahoo.de>
2024-07-28 20:44:31 +0200

    Add Description property

--- DeviceInterfaceManager/Models/IInputCreator.cs
+++ DeviceInterfaceManager/Models/IInputCreator.cs
@@ -5,27 +5,29 @@ namespace DeviceInterfaceManager.Models;
 
 public interface IInputCreator
 {
+    public string? Description { get; set; }
+    
     public string? InputType { get; set; }
 
     public Component? Input { get; set; }
 
     public string? EventType { get; set; }
 
-    public int? PmdgEvent { get; set; }
-
     public string? Event { get; set; }
 
-    public bool OnRelease { get; set; }
+    public long? DataPress { get; set; }
+    
+    public long? DataPress2 { get; set; }
+
+    public long? DataRelease { get; set; }
+    
+    public long? DataRelease2 { get; set; }
+
+    public int? PmdgEvent { get; set; }
 
     public Mouse? PmdgMousePress { get; set; }
 
     public Mouse? PmdgMouseRelease { get; set; }
 
-    public uint? DataPress { get; set; }
-    
-    public uint? DataPress2 { get; set; }
-
-    public uint? DataRelease { get; set; }
-    
-    public uint? DataRelease2 { get; set; }
+    public bool OnRelease { get; set; }
 }
