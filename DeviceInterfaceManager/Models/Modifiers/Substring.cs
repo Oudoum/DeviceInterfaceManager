c5053101e239429e92d424c20c73757e669d39cf
Nils Schmitt <oudoumo@yahoo.de>
2024-07-28 20:36:20 +0200

    Add Substring.cs modifier

---
+++ DeviceInterfaceManager/Models/Modifiers/Substring.cs
@@ -0,0 +1,28 @@
+using System.Text;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DeviceInterfaceManager.Models.Modifiers;
+
+public partial class Substring : ObservableObject, IModifier
+{
+    [ObservableProperty]
+    private bool _isActive = true;
+
+    [ObservableProperty]
+    private int _start;
+
+    [ObservableProperty]
+    private int _end = 7;
+
+    public void Apply(ref StringBuilder value)
+    {
+        if (Start >= value.Length)
+        {
+           value.Clear();
+           return;
+        }
+        
+        value.Remove(0, Start);
+        value.Length = End;
+    }
+}
