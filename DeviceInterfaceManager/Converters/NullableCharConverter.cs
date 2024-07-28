147330db70fe7d60bccaa89d6e0de39fa5cef666
Nils Schmitt <oudoumo@yahoo.de>
2024-07-28 20:37:58 +0200

    Add NullableCharConverter.cs

---
+++ DeviceInterfaceManager/Converters/NullableCharConverter.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace DeviceInterfaceManager.Converters;
+
+public class NullableCharConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return value?.ToString();
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (string.IsNullOrEmpty(value as string))
+        {
+            return null;
+        }
+        
+        return char.Parse((string)value);
+    }
+}
