d982a89e412da483bcab8c3da8ab057588dab84a
Nils Schmitt <oudoumo@yahoo.de>
2024-11-10 21:20:45 +0100

    Add Serilog as logger

--- DeviceInterfaceManager/Program.cs
+++ DeviceInterfaceManager/Program.cs
@@ -1,6 +1,9 @@
 ﻿using Avalonia;
 using System;
+using Avalonia.Logging;
+using Serilog;
 using Velopack;
+using LogEventLevel = Serilog.Events.LogEventLevel;
 
 namespace DeviceInterfaceManager;
 
@@ -13,13 +16,42 @@ internal static class Program
     public static void Main(string[] args)
     {
         VelopackApp.Build().Run();
-        BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+
+        try
+        {
+            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
+        }
+        catch (Exception e)
+        {
+            Log.Fatal(e, "Application terminated unexpectedly!");
+            throw;
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 
     // Avalonia configuration, don't remove; also used by visual designer.
     private static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
+    {
+        Log.Logger = new LoggerConfiguration()
+            .WriteTo.Console(LogEventLevel.Warning)
+            .WriteTo.File(App.UserDataPath + @"\Logs\Log.txt", LogEventLevel.Warning, rollingInterval: RollingInterval.Day)
+            .Filter.ByExcluding(logEvent => logEvent.MessageTemplate.Text.Contains("binding"))
+            .CreateLogger();
+
+        Log.Information("Starting up");
+
+        return AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont()
-            .LogToTrace();
+            .LogToSink(Log.Logger);
+    }
+
+    private static AppBuilder LogToSink(this AppBuilder builder, ILogger logger)
+    {
+        Logger.Sink = new SerilogSink(logger);
+        return builder;
+    }
 }
