21ac18434018997e73b5b472b1f0f4d34a082e39
Nils Schmitt <oudoumo@yahoo.de>
2024-11-10 20:49:42 +0100

    Change namespaces Change class name

---
+++ DeviceInterfaceManager/Views/Dialogs/AskComboBoxDialog.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DeviceInterfaceManager.Views.Dialogs;
+
+public partial class AskComboBoxDialog : UserControl
+{
+    public AskComboBoxDialog()
+    {
+        InitializeComponent();
+    }
+}
