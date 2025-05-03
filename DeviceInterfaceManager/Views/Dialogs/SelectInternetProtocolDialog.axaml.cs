ef6a08c2fc20a4e6e52e2c853fb320a4c5c2425e
Oudoum <77352752+Oudoum@users.noreply.github.com>
2025-05-03 22:43:35 +0200

    Add SelectInternetProtocolDialog.axaml.cs

---
+++ DeviceInterfaceManager/Views/Dialogs/SelectInternetProtocolDialog.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DeviceInterfaceManager.Views.Dialogs;
+
+public partial class SelectInternetProtocolDialog : UserControl
+{
+    public SelectInternetProtocolDialog()
+    {
+        InitializeComponent();
+    }
+}
