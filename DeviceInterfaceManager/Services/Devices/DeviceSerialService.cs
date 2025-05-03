906fa5e7459c37154e07a474c2ca12d8c41b10be
Oudoum <77352752+Oudoum@users.noreply.github.com>
2025-05-03 22:33:20 +0200

    Fix return type

--- DeviceInterfaceManager/Services/Devices/DeviceSerialService.cs
+++ DeviceInterfaceManager/Services/Devices/DeviceSerialService.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Avalonia;
@@ -48,8 +49,8 @@ public class DeviceSerialService : DeviceServiceBase
         return Task.FromResult(ConnectionStatus.Connected);
     }
 
-    public override void Disconnect()
+    public override Task Disconnect()
     {
-        
+        return Task.CompletedTask;
     }
 }
