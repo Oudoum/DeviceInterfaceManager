377246700882490949ac88fe31b4a370edca6d60
Oudoum <77352752+Oudoum@users.noreply.github.com>
2025-05-03 22:24:34 +0200

    Add validation

--- DeviceInterfaceManager/ViewModels/Dialogs/AskTextBoxDialogModel.cs
+++ DeviceInterfaceManager/ViewModels/Dialogs/AskTextBoxDialogModel.cs
@@ -1,10 +1,35 @@
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
 using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace DeviceInterfaceManager.ViewModels.Dialogs;
 
-public class AskTextBoxDialogModel : ObservableObject
+public class AskTextBoxDialogModel : ObservableValidator
 {
     public string? Title { get; set; }
 
+    [Required]
+    [CustomValidation(typeof(AskTextBoxDialogModel), nameof(ValidateFileName))]
     public string? Text { get; set; }
+
+    public static ValidationResult? ValidateFileName(string name, ValidationContext validationContext)
+    {
+        char[] invalidChars = Path.GetInvalidFileNameChars();
+        char[] invalidCharsInName = name.Where(ch => invalidChars.Contains(ch)).Distinct().ToArray();
+
+        if (invalidCharsInName.Length == 0)
+        {
+            return ValidationResult.Success;
+        }
+
+        string invalidCharsString = new(invalidCharsInName);
+        return new ValidationResult($"The file name contains invalid characters: {invalidCharsString}");
+    }
+
+    public bool CheckForErrors()
+    {
+        ValidateAllProperties();
+        return HasErrors;
+    }
 }
