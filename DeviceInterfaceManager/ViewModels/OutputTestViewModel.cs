d0aa2114fb629280a259e07121cdb1eec65a8eaa
Oudoum <77352752+Oudoum@users.noreply.github.com>
2025-05-03 23:22:33 +0200

    Add turn all outputs on and off Add setting seven segments Remove preprocessor directive Remove logger

--- DeviceInterfaceManager/ViewModels/OutputTestViewModel.cs
+++ DeviceInterfaceManager/ViewModels/OutputTestViewModel.cs
@@ -1,138 +1,149 @@
-﻿using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using DeviceInterfaceManager.Services.Devices;
-using Microsoft.Extensions.Logging;
 using Component = DeviceInterfaceManager.Models.Devices.Component;
 
 namespace DeviceInterfaceManager.ViewModels;
 
 public partial class OutputTestViewModel : ObservableObject
 {
-    private readonly ILogger _logger;
-    
-    public OutputTestViewModel(ILogger<OutputTestViewModel> logger, IDeviceService deviceService)
-    {
-        _logger = logger;
-        DeviceService = deviceService;
-        SelectedDataline = DeviceService.Outputs?.Dataline.Components.FirstOrDefault();
-        SelectedSevenSegment = DeviceService.Outputs?.SevenSegment.Components.FirstOrDefault();
-        SelectedAnalog = DeviceService.Outputs?.Analog.Components.FirstOrDefault();
-    }
-    
-#if DEBUG
-    public OutputTestViewModel()
-    {
-        _logger = new LoggerFactory().CreateLogger<OutputTestViewModel>();
-        DeviceService = new DeviceSerialService();
-    }
-#endif
-
     public IDeviceService DeviceService { get; }
 
-    [ObservableProperty]
-    private Component? _selectedDataline;
-
-    [RelayCommand]
-    private async Task SetDataline(bool direction)
+    public OutputTestViewModel(IDeviceService deviceService)
     {
-        if (SelectedDataline is null)
-        {
-            return;
-        }
-
-        await DeviceService.SetDatalineAsync(SelectedDataline.Position, direction);
-    }
-
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(SevenSegmentText))]
-    private Component? _selectedSevenSegment;
-
-    partial void OnSelectedSevenSegmentChanged(Component? value)
-    {
-        if (value is null)
-        {
-            return;
-        }
-
-        Reset7SegmentDisplay();
-        SevenSegmentText = _sevenSegmentText;
-    }
-
-    private string _sevenSegmentText = string.Empty;
-
-    public string SevenSegmentText
-    {
-        get => _sevenSegmentText;
-        set
-        {
-            if (DeviceService.Outputs is null || SelectedSevenSegment is null)
-            {
-                return;
-            }
-
-            if (SevenSegmentText.Length > value.Length)
-            {
-                Reset7SegmentDisplay();
-            }
-
-            int length = DeviceService.Outputs.SevenSegment.Count - SelectedSevenSegment.Position + DeviceService.Outputs.SevenSegment.First;
-            if (value.Length > length)
-            {
-                _sevenSegmentText = value.Remove(length);
-                DeviceService.SetSevenSegmentAsync(SelectedSevenSegment.Position, _sevenSegmentText);
-                return;
-            }
-
-            DeviceService.SetSevenSegmentAsync(SelectedSevenSegment.Position, _sevenSegmentText = value);
-        }
-    }
-
-    private void Reset7SegmentDisplay()
-    {
-        if (DeviceService.Outputs is null)
-        {
-            return;
-        }
-
-        DeviceService.SetSevenSegmentAsync(DeviceService.Outputs.SevenSegment.First, new string(' ', DeviceService.Outputs.SevenSegment.Count));
-    }
-
-    [ObservableProperty]
-    private Component? _selectedAnalog;
-
-    [ObservableProperty]
-    private int _analogValue;
-
-    partial void OnAnalogValueChanged(int value)
-    {
-        if (DeviceService.Outputs is null || SelectedAnalog is null)
-        {
-            return;
-        }
-        
-        DeviceService.SetAnalogAsync(SelectedAnalog.Position, value);
+        DeviceService = deviceService;
     }
 
     [RelayCommand]
-    private async Task IsChecked(Component component)
+    private async Task TurnLedOnOff(Component component)
     {
         await DeviceService.SetLedAsync(component.Position, component.IsSet);
     }
 
     [RelayCommand]
-    private async Task SetAllLed(bool direction)
+    private async Task TurnAllLedsOn()
+    {
+        await SetAllLeds(true);
+    }
+
+    [RelayCommand]
+    private async Task TurnAllLedsOff()
+    {
+        await SetAllLeds(false);
+    }
+
+    private async Task SetAllLeds(bool direction)
     {
         if (DeviceService.Outputs is null)
         {
             return;
         }
-        
+
         foreach (Component ledComponent in DeviceService.Outputs.Led.Components)
         {
             ledComponent.IsSet = direction;
             await DeviceService.SetLedAsync(ledComponent.Position, direction);
+        }
+    }
+
+    [RelayCommand]
+    private async Task TurnDatalineOnOff(Component component)
+    {
+        await DeviceService.SetDatalineAsync(component.Position, component.IsSet);
+    }
+
+    [RelayCommand]
+    private async Task TurnAllDatalinesOn()
+    {
+        await SetAllDatalines(true);
+    }
+
+    [RelayCommand]
+    private async Task TurnAllDatalinesOff()
+    {
+        await SetAllDatalines(false);
+    }
+
+    private async Task SetAllDatalines(bool direction)
+    {
+        if (DeviceService.Outputs is null)
+        {
+            return;
+        }
+
+        foreach (Component datalineComponent in DeviceService.Outputs.Dataline.Components)
+        {
+            datalineComponent.IsSet = direction;
+            await DeviceService.SetDatalineAsync(datalineComponent.Position, direction);
+        }
+    }
+
+    [RelayCommand]
+    private async Task SetAnalog(Component component)
+    {
+        await DeviceService.SetAnalogAsync(component.Position, component.Value);
+    }
+
+    [RelayCommand]
+    private async Task SetAllAnalogsMax()
+    {
+        await SetAllAnalogs(255);
+    }
+
+    [RelayCommand]
+    private async Task SetAllAnalogsMin()
+    {
+        await SetAllAnalogs(0);
+    }
+
+    private async Task SetAllAnalogs(int value)
+    {
+        if (DeviceService.Outputs is null)
+        {
+            return;
+        }
+
+        foreach (Component analogComponent in DeviceService.Outputs.Analog.Components)
+        {
+            analogComponent.Value = value;
+            await DeviceService.SetAnalogAsync(analogComponent.Position, value);
+        }
+    }
+
+    [RelayCommand]
+    private async Task SetSevenSegment(Component component)
+    {
+        if (component.StringValue is null)
+        {
+            return;
+        }
+
+        await DeviceService.SetSevenSegmentAsync(component.Position, component.StringValue);
+    }
+
+    [RelayCommand]
+    private async Task FillAllSevenSegments()
+    {
+        await SetAllSevenSegments("8");
+    }
+    
+    [RelayCommand]
+    private async Task ClearAllSevenSegments()
+    {
+        await SetAllSevenSegments(" ");
+    }
+
+    private async Task SetAllSevenSegments(string value)
+    {
+        if (DeviceService.Outputs is null)
+        {
+            return;
+        }
+
+        foreach (Component segmentComponent in DeviceService.Outputs.SevenSegment.Components)
+        {
+            await DeviceService.SetSevenSegmentAsync(segmentComponent.Position, value);
         }
     }
 }
