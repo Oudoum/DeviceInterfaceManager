87eab890edd0ef73f5aec61c9f6ead7a67fdd48e
Oudoum <77352752+Oudoum@users.noreply.github.com>
2025-05-03 23:01:49 +0200

    Remove preprocessor directive

--- DeviceInterfaceManager/ViewModels/InputTestViewModel.cs
+++ DeviceInterfaceManager/ViewModels/InputTestViewModel.cs
@@ -1,5 +1,4 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
-using DeviceInterfaceManager.Models.Devices;
 using DeviceInterfaceManager.Services.Devices;
 
 namespace DeviceInterfaceManager.ViewModels;
@@ -10,13 +9,6 @@ public class InputTestViewModel : ObservableObject
     {
         DeviceService = deviceService;
     }
-    
-#if DEBUG
-    public InputTestViewModel()
-    {
-        DeviceService = new DeviceSerialService();
-    }
-#endif
-
+ 
     public IDeviceService DeviceService { get; }
 }
