608add435be9cbc7aaae41ec3076f024f18269e2
Oudoum <77352752+Oudoum@users.noreply.github.com>
2025-12-19 16:21:33 +0100

    Add logging

--- DeviceInterfaceManager/ViewModels/Dialogs/SelectInternetProtocolDialogModel.cs
+++ DeviceInterfaceManager/ViewModels/Dialogs/SelectInternetProtocolDialogModel.cs
@@ -3,15 +3,23 @@ using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.Input;
 using DeviceInterfaceManager.Models;
 using DeviceInterfaceManager.Services.Devices.Fds;
+using Microsoft.Extensions.Logging;
 
 namespace DeviceInterfaceManager.ViewModels.Dialogs;
 
 public partial class SelectInternetProtocolDialogModel : SelectConnectionDialogModel
 {
+    private readonly ILogger _logger;
+
+    public SelectInternetProtocolDialogModel(ILogger<SelectInternetProtocolDialogModel> logger)
+    {
+        _logger = logger;
+    }
+
     [RelayCommand(CanExecute = nameof(CanSearchForDevices))]
     private async Task SearchForDevicesAsync()
     {
-        var connections = await InterfaceItEthernetService.ReceiveControllerDiscoveryDataAsync();
+        var connections = await InterfaceItEthernetService.ReceiveControllerDiscoveryDataAsync(_logger);
         foreach (string connection in connections.Where(connection => !string.IsNullOrEmpty(connection)))
         {
             if (PreSelectedConnection is null)
