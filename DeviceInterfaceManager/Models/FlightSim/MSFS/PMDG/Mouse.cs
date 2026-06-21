388ee582c3132833c553360438aba43dd9cbf130
Oudoum <77352752+Oudoum@users.noreply.github.com>
2026-06-21 20:43:22 +0200

    Change to int64

--- DeviceInterfaceManager/Models/FlightSim/MSFS/PMDG/Mouse.cs
+++ DeviceInterfaceManager/Models/FlightSim/MSFS/PMDG/Mouse.cs
@@ -1,23 +1,23 @@
 namespace DeviceInterfaceManager.Models.FlightSim.MSFS.PMDG;
 
-public enum Mouse : uint
+public enum Mouse : long
 {
-    RightSingle = 0x80000000u,
-    // MiddleSingle = 0x40000000u,
-    LeftSingle = 0x20000000u,
-    // RightDouble = 0x10000000u,
-    // MiddleDouble = 0x08000000u,
-    // LeftDouble = 0x04000000u,
-    // RightDrag = 0x02000000u,
-    // MiddleDrag = 0x01000000u,
-    // LeftDrag = 0x00800000u,
-    // Move = 0x00400000u,
-    // DownRepeat = 0x00200000u,
-    RightRelease = 0x00080000u,
-    // MiddleRelease = 0x00040000u,
-    LeftRelease = 0x00020000u,
-    // WheelFlip = 0x00010000u, // invert direction of mouse wheel
-    // WheelSkip = 0x00008000u, // look at next 2 rect for mouse wheel commands
-    WheelUp = 0x00004000u,
-    WheelDown = 0x00002000u
+    RightSingle = 0x80000000,
+    // MiddleSingle = 0x40000000,
+    LeftSingle = 0x20000000,
+    // RightDouble = 0x10000000,
+    // MiddleDouble = 0x08000000,
+    // LeftDouble = 0x04000000,
+    // RightDrag = 0x02000000,
+    // MiddleDrag = 0x01000000,
+    // LeftDrag = 0x00800000,
+    // Move = 0x00400000,
+    // DownRepeat = 0x00200000,
+    RightRelease = 0x00080000,
+    // MiddleRelease = 0x00040000,
+    LeftRelease = 0x00020000,
+    // WheelFlip = 0x00010000, // invert direction of mouse wheel
+    // WheelSkip = 0x00008000, // look at next 2 rect for mouse wheel commands
+    WheelUp = 0x00004000,
+    WheelDown = 0x00002000
 }
